1e4496e160a28404e9d937ec3e6e8729221626cd
Stephen Yang <isstephen@gmail.com>
2025-03-30 21:30:54 +1300

    update the terraform-key path

--- terraform-manifests/c9-nullresource-provisioners.tf
+++ terraform-manifests/c9-nullresource-provisioners.tf
@@ -1,32 +1,22 @@
-# Create a Null Resource and Provisioners
 resource "null_resource" "name" {
   depends_on = [module.ec2-public, module.vpc]
-  # Connection Block for Provisioners to connect to EC2 Instance
+
   connection {
     type        = "ssh"
     host        = aws_eip.bastion_eip.public_ip
     user        = "ec2-user"
     private_key = file("${path.module}/private-key/terraform-key.pem")
-    agent        = false
+    agent       = false
   }
 
-provisioner "remote-exec" {
-  inline = [
-    // Write the contents via sudo, using "tee"
-    "echo '${file("/Users/yangyang/repos/terraform-on-aws/07-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/private-key/terraform-key.pem")}' | sudo tee /home/ec2-user/terraform-key.pem",
+  provisioner "remote-exec" {
+    inline = [
+      # Write the contents of the key to a file on the remote EC2 instance
+      "echo '${replace(file("${path.module}/private-key/terraform-key.pem"), "\n", "\\n")}' | base64 -d | sudo tee /home/ec2-user/terraform-key.pem > /dev/null",
 
-    // Set correct ownership and permissions
-    "sudo chown ec2-user:ec2-user /home/ec2-user/terraform-key.pem",
-    "sudo chmod 400 /home/ec2-user/terraform-key.pem"
-  ]
-}
-  /*
-  ## local Exec provisioner (destroy time provisioner - trigger during deletion of Resource)
-  provisioner "local-exec" {
-    command     = "echo create time prov `date`: >> destroy-time-provid.txt"
-    working_dir = "local-exec-output-files/"
-    #when = destroy
-    #on_failure = continue
+      # Set correct ownership and permissions
+      "sudo chown ec2-user:ec2-user /home/ec2-user/terraform-key.pem",
+      "sudo chmod 400 /home/ec2-user/terraform-key.pem"
+    ]
   }
-  */
 }
